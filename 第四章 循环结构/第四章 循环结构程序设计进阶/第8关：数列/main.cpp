4b97fd6eb443ec77cbc93a04cf3f2fa1d10d496c
Joseph Lim <mahironya@icloud.com>
2025-03-21 16:50:39 +0800

    updated answer(s)

---
+++ 第四章 循环结构/第四章 循环结构程序设计进阶/第8关：数列/main.cpp
@@ -0,0 +1,15 @@
+/*
+ 1 2 6 24 120
+ */
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main(){
+    double x, sum = 1;
+    int times;
+    cin >> x >> times;
+    int count = 0;// 因为数据集中 times 都大于 15
+    for (int i = 2; i <= times; ++i){
+        
+    }
+}
