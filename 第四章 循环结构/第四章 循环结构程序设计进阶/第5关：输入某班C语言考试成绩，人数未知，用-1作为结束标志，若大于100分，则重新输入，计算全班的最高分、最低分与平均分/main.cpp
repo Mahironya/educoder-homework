a1cd98107f4c3642f3e0457fc96d315ac89fb061
Joseph Lim <mahironya@icloud.com>
2025-03-21 12:52:45 +0800

    123

---
+++ 第四章 循环结构/第四章 循环结构程序设计进阶/第5关：输入某班C语言考试成绩，人数未知，用-1作为结束标志，若大于100分，则重新输入，计算全班的最高分、最低分与平均分/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <climits>
+#include <iomanip>
+using namespace std;
+int main(){
+    float score_input, max_score, min_score;
+    max_score = INT_MIN;
+    min_score = INT_MAX;
+    
+}
