01db5f8fd8762141364e23f3bd2122f91ea372cb
Joseph Lim <mahironya@icloud.com>
2025-03-24 21:50:50 +0800

    update

--- 第四章 循环结构/第四章 循环结构程序设计进阶/第9关：进制转换/main.cpp
+++ 第四章 循环结构/第四章 循环结构程序设计进阶/第9关：进制转换/main.cpp
@@ -1,1 +1,22 @@
-#in
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main(){
+    string hex = "";
+    string trans[6] = {"A","B","C","D","E","F"}; //store letters
+    int num, temp = 0;
+    cin >> num;
+    do {
+        temp = num % 16;
+        if (temp >= 10 && temp <= 15){
+            hex += trans[temp - 10];
+        }else{
+            hex += to_string(temp);
+        }
+        num /= 16;
+    } while (num > 0);
+    reverse(hex.begin(), hex.end());
+    
+    cout << hex <<endl;
+}
