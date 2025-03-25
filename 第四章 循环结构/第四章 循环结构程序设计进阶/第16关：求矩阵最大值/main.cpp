f8a07073c9e1f75d89e65c9e323b9dcced23ff53
Joseph Lim <mahironya@icloud.com>
2025-03-25 16:54:15 +0800

    update

--- 第四章 循环结构/第四章 循环结构程序设计进阶/第16关：求矩阵最大值/main.cpp
+++ 第四章 循环结构/第四章 循环结构程序设计进阶/第16关：求矩阵最大值/main.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+int main(){
+    int row, column, num, record_row = 0, record_column = 0;
+    int max_num = INT_MIN;
 
+    
+    cin >> row >> column;
+    for (int i = 1; i <= row; ++i){
+        for (int j = 1; j <= column; ++j){
+            cin >> num;
+            if (num > max_num){
+                max_num = num;
+                record_row = i;
+                record_column = j;
+            }
+        }
+    }
+    cout <<"该"<<row<<"*"<<column<<"矩阵的最大元素信息如下："<<endl;
+    cout<<"max="<<max_num<<endl;
+    cout<<"row="<<record_row<<endl;
+    cout<<"column="<<record_column;
+    
+}
---
+++ 第四章 循环结构/第四章 循环结构程序设计进阶/第17关：逆序输出数组/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  第17关：逆序输出数组
+//
+//  Created by Joseph Lee on 3/25/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
