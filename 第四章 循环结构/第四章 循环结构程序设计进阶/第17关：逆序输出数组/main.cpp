3d415b3845e0047dbf7d166b269fe38b548dd1b5
Joseph Lim <mahironya@icloud.com>
2025-03-25 18:42:51 +0800

    update

--- 第四章 循环结构/第四章 循环结构程序设计进阶/第17关：逆序输出数组/main.cpp
+++ 第四章 循环结构/第四章 循环结构程序设计进阶/第17关：逆序输出数组/main.cpp
@@ -1,14 +1,25 @@
-//
-//  main.cpp
-//  第17关：逆序输出数组
-//
-//  Created by Joseph Lee on 3/25/25.
-//
-
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    vector<int> store;
+    int num;
+
+    for (int i = 0; i < 5; ++i) {
+        cin >> num;
+        store.push_back(num);
+    }
+
+    reverse(store.begin(), store.end());
+
+    // 确保每个数都占 5 格宽度
+    for (int i = 0; i < 5; ++i) {
+        cout << setw(5) << store[i];
+    }
+
+    cout << endl; // 换行
     return 0;
 }
