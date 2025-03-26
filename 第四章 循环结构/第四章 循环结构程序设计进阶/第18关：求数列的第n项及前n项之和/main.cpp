cffada8b75d8a17fd80f29da351df5ed40f89cd1
Joseph Lim <mahironya@icloud.com>
2025-03-26 13:31:58 +0800

    update

--- 第四章 循环结构/第四章 循环结构程序设计进阶/第18关：求数列的第n项及前n项之和/main.cpp
+++ 第四章 循环结构/第四章 循环结构程序设计进阶/第18关：求数列的第n项及前n项之和/main.cpp
@@ -1,1 +1,15 @@
-
+#include<iostream>
+using namespace std;
+int sequence(int num_input){
+    if (num_input == 1 || num_input == 2) return 1;
+    return 3*(sequence(num_input - 1)) + 2*sequence(num_input - 2);
+}
+int main(){
+    int num, sum = 0;
+    cin >> num;
+    cout<<sequence(num)<<endl;
+    for (int i = 1; i <= num; ++i){
+        sum += sequence(i);
+    }
+    cout<<sum;
+}
