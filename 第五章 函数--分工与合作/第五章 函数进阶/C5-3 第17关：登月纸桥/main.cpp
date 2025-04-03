8981564781c954354ef4919136b2cc046bcd645d
Oyama Mahiro 🍥 <mahironya@icloud.com>
2025-04-03 20:24:40 +0800

    Update main.cpp

--- 第五章 函数--分工与合作/第五章 函数进阶/C5-3 第17关：登月纸桥/main.cpp
+++ 第五章 函数--分工与合作/第五章 函数进阶/C5-3 第17关：登月纸桥/main.cpp
@@ -1,2 +1,13 @@
 #include<iostream>
+#include<iomanip>
+#include<cmath>
 using namespace std;
+double output_log_base2(double num){
+    return log(num)/log(2.0);
+}
+int main(){
+    double height, thickness;
+    cin >> height >> thickness;
+    height *= 1e6;
+    cout<<"需要折叠"<<ceil(output_log_base2(height/thickness))<<"次"<<endl;
+}
