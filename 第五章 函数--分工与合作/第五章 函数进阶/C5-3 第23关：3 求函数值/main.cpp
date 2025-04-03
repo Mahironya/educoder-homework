6cddced8ffa5e7249c265c2653c8346fa8757dc1
Oyama Mahiro 🍥 <mahironya@icloud.com>
2025-04-03 20:01:56 +0800

    update

---
+++ 第五章 函数--分工与合作/第五章 函数进阶/C5-3 第23关：3 求函数值/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C5-3 第23关：3 求函数值
+//
+//  Created by Joseph Lee on 4/1/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
