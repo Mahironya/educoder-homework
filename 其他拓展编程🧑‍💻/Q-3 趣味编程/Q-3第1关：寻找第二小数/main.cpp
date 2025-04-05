642bdbb89956d1f9c71c39c11baf699dd8f41fa8
Oyama Mahiro 🍥 <mahironya@icloud.com>
2025-04-05 22:45:36 +0800

    update

--- 其他拓展编程🧑‍💻/Q-3 趣味编程/Q-3第1关：寻找第二小数/main.cpp
+++ 其他拓展编程🧑‍💻/Q-3 趣味编程/Q-3第1关：寻找第二小数/main.cpp
@@ -3,7 +3,8 @@ using namespace std;
 int find_mid(int a, int b, int c){
     if (a>b) swap(a,b);
     if (b>c) swap(b,c);
-    if (a>c) swap(a,c);
+    if (a>b) swap(a,b);
+    return b;
 }
 int main(){
     int a, b, c;
