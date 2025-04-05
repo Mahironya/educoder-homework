558a7965280df75653e8b6594dfe773fa8d5260e
Oyama Mahiro 🍥 <mahironya@icloud.com>
2025-04-05 15:50:34 +0800

    rename

---
+++ 其他拓展编程🧑‍💻/Q-3 趣味编程/Q-3第1关：寻找第二小数/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int find_mid(int a, int b, int c){
+    if (a>b) swap(a,b);
+    if (b>c) swap(b,c);
+    if (a>c) swap(a,c);
+}
+int main(){
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout<<find_mid(a, b, c);
+}
