8c37dbd87d226fd148388d43a151907e37a48a32
Oyama Mahiro 🍥 <mahironya@icloud.com>
2025-04-15 20:27:01 +0800

    upd

--- 第六章 用数组处理批量数据/C6-1 数组/C6-1 第5关：冒泡排序/main.cpp
+++ 第六章 用数组处理批量数据/C6-1 数组/C6-1 第5关：冒泡排序/main.cpp
@@ -1,14 +1,29 @@
-//
-//  main.cpp
-//  C6-1 第5关：冒泡排序
-//
-//  Created by Joseph Lee on 4/15/25.
-//
-
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+#include<iostream>
+using namespace std;
+void printsequence(int sequence[], int size){
+    for (int i = 0; i<= size - 1; ++i){
+        cout<<sequence[i]<<" ";
+    }
+}
+int main(){
+    int length = 0;
+    cin >> length;
+    int pop_num[length];
+    for (int i = 0; i<length; ++i){
+        int num_input;
+        cin >> num_input;
+        pop_num[i] = num_input;
+    }
+    for (int i = 0; i < length; ++i){
+        for (int j = 0; j < length-i-1; ++j){
+//            int pointer = 1+j;
+            if (pop_num[j] > pop_num[j+1]){
+                swap(pop_num[j], pop_num[j+1]);
+            }
+        }
+        if (i<length-1){
+            printsequence(pop_num, length);
+            cout<<endl;
+        }
+    }
 }
---
+++ 第六章 用数组处理批量数据/C6-1 数组/C6-1 第6关：编写程序将一个给定的整数插到原本有序的整数序列中，使结果序列仍然有序/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C6-1 第6关：编写程序将一个给定的整数插到原本有序的整数序列中，使结果序列仍然有序
+//
+//  Created by Joseph Lee on 4/15/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
