f5535128243ad0ad942404acf87857be2beccbed
Oyama Mahiro 🍥 <mahironya@icloud.com>
2025-04-15 11:03:40 +0800

    upd

--- 第六章 用数组处理批量数据/C6-1 数组/C6-1 第3关：用筛选法求100以内所有素数的和/main.cpp
+++ 第六章 用数组处理批量数据/C6-1 数组/C6-1 第3关：用筛选法求100以内所有素数的和/main.cpp
@@ -1,14 +1,40 @@
-//
-//  main.cpp
-//  C6-1 第3关：用筛选法求100以内所有素数的和
-//
-//  Created by Joseph Lee on 4/15/25.
-//
-
 #include <iostream>
+#include <vector>
+using namespace std;
+void printPrimes(int n) {
+    vector<int> a(n + 1);
+    int count = 0;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    // 初始化，从 2 到 n
+    for (int i = 2; i <= n; i++) {
+        a[i] = i;
+    }
+
+    int i = 2;
+    while (i < n) {
+        // 把当前素数的倍数筛掉
+        for (int j = a[i] * 2; j <= n; j += a[i]) {
+            a[j] = 0;
+        }
+        i++;
+        while (i <= n && a[i] == 0) {
+            i++;
+        }
+    }
+
+    // 统计剩下的素数数量
+    int helper = 0;
+    for (int i = 2; i <= n; i++) {
+        if (a[i] != 0) {
+            helper++;
+            cout<<a[i]<<"\t";
+            if (helper%10 == 0){
+                cout<<endl;
+            }
+        }
+    }
+
+}
+int main(){
+    printPrimes(100);
 }
