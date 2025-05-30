08e487644bcb74a2fb23b8b94766d58a127573d8
Oyama Mahiro 🍥 <mahironya@icloud.com>
2025-05-30 16:37:33 +0800

    upd

--- 第六章 用数组处理批量数据/C6-2 数组进阶/C6-2 第13关：将两个有序的数组合并成一个有序数组/main.cpp
+++ 第六章 用数组处理批量数据/C6-2 数组进阶/C6-2 第13关：将两个有序的数组合并成一个有序数组/main.cpp
@@ -2,69 +2,53 @@
 #include<iostream>
 using namespace std;
 #define N 20
-int main()
-{
 
-    int a[N]={ 0 }, b[N]={ 0 };
-    int i,j,k,anum,bnum,c[N+N] = {0};
-    scanf("%d",&anum);   //输入数组a元素个数
-    for(i=0;i<anum;i++)
-    {
-        scanf("%d",&a[i]);
-    }
-    scanf("%d",&bnum);   //输入数组b元素个数
-    for(i=0;i<bnum;i++)
-    {
-        scanf("%d",&b[i]);
-    }
-    /***** 在数组a和b都有数据时比较两个数组 *****/
-    /********** Begin *********/
-    i = j = k = 0;
-    // c = {0,0,0,0,...,0,0}
-    while (i < anum && j < bnum){
-        if (a[i] <= b[j]){
-            c[k] = a[i];
-            k++;
-            i++;
+void combine(int *a, int *b, int lena, int lenb, int *c);
 
-        }else{
-            c[k] = b[j];
-            j++;
-            k++;
+int main() {
+    int a[N] = {0}, b[N] = {0}, c[N+N] = {0};
+    int i, anum, bnum;
 
-        }
+    scanf("%d", &anum);
+    for(i = 0; i < anum; i++) {
+        scanf("%d", &a[i]);
     }
-    
-    /********** End **********/
-    /***** 如果数组a还有数据 *****/
-    /********** Begin *********/
-    
-    // a的指针还是为i
-    while (anum - i > 0){
-        c[k] = a[i];
-        k++;
-        i++;
 
+    scanf("%d", &bnum);
+    for(i = 0; i < bnum; i++) {
+        scanf("%d", &b[i]);
     }
-    
-    /********** End **********/
 
-    /***** 如果数组b还有数据 *****/
-    /********** Begin *********/
-    while (bnum - j > 0){
-        c[k] = b[j];
-        k++;
-        j++;
+    combine(a, b, anum, bnum, c);
 
+    for (int i = 0; i < anum + bnum; i++) {
+        cout << c[i] << " ";
     }
-    
-    /********** End **********/
-    /***** 输出数组c *****/
-    /********** Begin *********/
-    for (int i = 0; i < anum + bnum ; i++){
-        cout<< c[i] <<" ";
-    }
-    
-    /********** End **********/
+
     return 0;
 }
+
+void combine(int a[], int b[], int lena, int lenb, int c[]) {
+    int i = 0, j = 0, k = 0;
+
+    while (i < lena && j < lenb) {
+        if (a[i] <= b[j]) {
+             
+            c[k++] = a[i++];
+        } else {
+            c[k++] = b[j++];
+             
+        }
+    }
+
+    while (i < lena) {
+        c[k++] = a[i++];
+        
+    }
+
+    while (j < lenb) {
+        c[k++] = b[j++];
+        
+    }
+}
+
